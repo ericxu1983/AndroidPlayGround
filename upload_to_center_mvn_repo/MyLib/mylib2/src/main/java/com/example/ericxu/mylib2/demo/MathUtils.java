8fdf8025dfd478e11ae75ce9c158d63d6cd5e4b4
kunlun <kunlun@qima-inc.com>
2015-04-02 20:01:29 +0800

    1. restore upload_to_center_mvn_repo gradle codes which were lost during uploading into github 2.add documents

---
+++ upload_to_center_mvn_repo/MyLib/mylib2/src/main/java/com/example/ericxu/mylib2/demo/MathUtils.java
@@ -0,0 +1,14 @@
+package com.example.ericxu.mylib2.demo;
+
+/**
+ * Created by ericxu on 15/4/2.
+ */
+public class MathUtils {
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+
+    public static int always99() {
+        return 99;
+    }
+}
